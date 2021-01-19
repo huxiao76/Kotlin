6cc9c2a5cf6e6cb6aa7e55c2a6feeedafccb4219
huxiao <xiao.hu@thoughtworks.com>
2021-01-19 16:31:20 +0800

    [xiao]: Interface

---
+++ src/com/thoughtworks/kotlin/Interface.kt
@@ -0,0 +1,25 @@
+package com.thoughtworks.kotlin
+
+interface A {
+    fun method() {
+        println("A")
+    }
+}
+
+open class B {
+    open fun method() {
+        println("B")
+    }
+}
+
+class C: A, B(){
+    override fun method() {
+        super<A>.method()
+        super<B>.method()
+    }
+}
+
+fun main() {
+    var c = C()
+    c.method()
+}
