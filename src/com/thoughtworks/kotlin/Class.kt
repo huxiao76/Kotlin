cdcafca81bd9531d15b5a25641675319e0892a12
huxiao <xiao.hu@thoughtworks.com>
2021-01-19 14:59:57 +0800

    [xiao]: construct new knowage

--- src/com/thoughtworks/kotlin/Class.kt
+++ src/com/thoughtworks/kotlin/Class.kt
@@ -6,6 +6,7 @@ class EmptyClass
 // primary构造方法是类头（class header）的一部分，它位于类名后面，可以拥有若干参数
 // 如果primary构造方法没有任何注解，或者可见性关键字修饰，那么constructor关键字可省略
 // secondary构造方法必须直接或间接的调用primary构造方法
+// 属性可以写在构造方法里面
 
 class MyClass constructor(){
 
@@ -33,7 +34,29 @@ class MyClass2 (username: String){
     }
 }
 
+class Student (private val username: String, private val age: Int, private var address: String) {
+    fun printInfo() {
+        println("username: $username, age: $age, address: $address")
+    }
+}
+
+/**
+ * 如果构造方法拥有注解或者可见性修饰符
+ * 那么constructor关键字就是不能省略掉的，并且它位于修饰符后面
+ */
+
+class Student2 private constructor(username: String) {
+
+}
+/**
+ * 在JVM里，如果primary构造方法的所有参数都拥有默认值，编译器则生成一新的个不带参数的构造方法，
+ * 这个不带参数的构造方法会使用这些参数的默认值，这样做的目的在于可以跟Spring等框架更好地集成
+ */
+class Student3(val username: String = "zhangsan")
+
 fun main() {
     var myClass = MyClass2("zhangsan", 20, "beijing");
     myClass.printInfo()
+    var student = Student("zhangsan", 20, "beijing")
+    student.printInfo()
 }
