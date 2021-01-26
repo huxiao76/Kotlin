062322064b7d8616ff68dd10e8f8e1d4d556045b
huxiao <xiao.hu@thoughtworks.com>
2021-01-26 18:32:00 +0800

    [xiao]: add Nested define

--- src/com/thoughtworks/kotlin2/NestedClass.kt
+++ src/com/thoughtworks/kotlin2/NestedClass.kt
@@ -1,12 +1,17 @@
 package com.thoughtworks.kotlin2
 
 // 嵌套类(Nested Class)
+// 嵌套类不能访问外部类的其他成员，只能访问另一个嵌套类
 
 class OuterClass {
     private val str: String = "hello world"
 
     class NestedClass {
         fun nestedMethod() = "welcome"
+    }
+
+    class NestedClass2 {
+        val nestedClass = NestedClass()
     }
 }
 
