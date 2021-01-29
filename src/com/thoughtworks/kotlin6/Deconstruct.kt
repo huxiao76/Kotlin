e27faff8418fd0d4c4d7496b64c73883c3ec8a9b
huxiao <xiao.hu@thoughtworks.com>
2021-01-29 14:55:59 +0800

    [xiao]: map deconstruct

--- src/com/thoughtworks/kotlin6/Deconstruct.kt
+++ src/com/thoughtworks/kotlin6/Deconstruct.kt
@@ -22,6 +22,27 @@ fun main() {
     println(result2)
     println(status2)
 
+    val map = mapOf("a" to "aa", "b" to "bb", "c" to "cc")
+    for ((key, value) in map) {
+        println("key $key, value: $value")
+    }
+
+    println("----------")
+    map.mapValues { entry -> "${entry.value} hello" }.forEach{ println(it) }
+    println("------------")
+
+    map.mapValues { (_, value) -> "$value world" }.forEach{ println(it)}
+    println("------------")
+
+    // kotlin 允许我们为解构声明整体指定类型，也可以为每一个具体的component指定类型
+    map.mapValues { (_, value): Map.Entry<String, String> -> "$value person" }.forEach{ println(it)}
+    println("------------")
+
+    map.mapValues { (_, value: String) -> "$value people" }.forEach{ println(it)}
+
+
 
 
 }
+
+
